fc874ccab6b8700680f15d0e8eb7284f8992f2fd
Onur DÖKER <odoker@msn.com>
2025-10-02 22:42:24 +0300

    Simplify global styles, remove unused CSS, adjust body layout, and clean up main.jsx by removing unused Header component.

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,11 @@ import { BrowserRouter } from "react-router";
 import "./index.css";
 
 import App from "./App.jsx";
-import Header from "./contents/Header/Header.jsx";
 
 createRoot(document.getElementById("root"))
     .render(
         <StrictMode>
           <BrowserRouter>
-            <Header />
             <App />
           </BrowserRouter>
         </StrictMode>,
